3727d25be076c68304afb943bd5c9d3504189806
asr1 <alexandersrinehart@gmail.com>
2015-06-23 19:36:57 -0700

    Fixed registration

--- Registration/registration.js
+++ Registration/registration.js
@@ -1,6 +1,8 @@
 var strPage = $("td[align='left'][class='sfff'][width='40%']").children().text(); //Used to determine if on class registration page
 var strPage2 = $("div[id='appDiv']").children("h2").text(); //Used to determine if on add/drop page
-if(strPage2.substring(0,8) === "Add/Drop" && strPage === "Class Registration")
+var strPage3 = window.location.href; //The URL we are on.
+var appPage = "https://accessplus.iastate.edu/servlet/adp.A_Plus"; //The registration applet
+if((strPage2.substring(0,8) === "Add/Drop" && strPage === "Class Registration") || (strPage === "Class Schedule" && strPage3 == appPage))
 {
 	//$("input[name='applicationSize']").next("tr").children().children("table").next().append("<div id='insert_schedule_here'></div>");
 	$("table[summary='Tab selection and application display area']").append("<div id='insert_schedule_here'></div>");
