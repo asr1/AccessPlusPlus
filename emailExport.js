aedc9e605f66dd2453624a759b04a0871b123224
asr1 <alexandersrinehart@gmail.com>
2016-02-20 00:09:21 -0600

    Infinite loop

--- emailExport.js
+++ emailExport.js
@@ -5,36 +5,64 @@ var accessPlus = "https://accessplus.iastate.edu/RH05/RH958"; //possible url for
 var accessPlus2 = "https://accessplus.iastate.edu/servlet/adp.A_Plus";
  if (url.includes(accessPlus) || url == accessPlus2)
  {
-	$("body").prepend('<input type="button" value="Export Emails">').click(function(){ 
+	$("body").prepend('<input type="button" id="exportBtn" value="Export Emails">');
+		
+		$("#exportBtn").click(function(){
+
 		//Iterate through each cell in the third row
 		var table = $("table:visible"); 
 		table = table[7];
+		var done = false;
+		
 		
 		var exportStr = "";
 		
-		//All on first page
-		for (var i = 0, row; row = table.rows[i]; i++)
+	
+		checkPage();
+
+		//Once we're done with all links, let's see if the page is done.
+		
+		//Iterate through each link
+		$("a").each(function() {
+		 	if($(this).context.innerHTML.includes("Next"))
+			{
+				console.log("hit");
+				console.log($(this));
+				alert($(this).context.innerHTML);
+				$(this).click();
+		})
+
+		//Check the second page
+		//This is so kludgey I'm sorry.
+		//It's better than what we had before, I promsie.
+		checkPage();
+		
+		
+		//Download the result
+		var blob = new Blob([exportStr], {
+			type: "text/plain;charset=utf-8;",
+		});
+			saveAs(blob, "emails.csv");
+		alert("Attention: please make sure to run this script on every page of housemates. Mailing lists can be made form this script as asw.iastate.edu");
+	});
+ }
+ 
+ 
+ function checkPage()
+ {
+	 for (var i = 0, row; row = table.rows[i]; i++)
 		{
 			for (var j = 0, col; col = row.cells[j]; j++)
 			{
 				var str = col.innerHTML;
 				var addr = str.split(" ");
-				console.log(addr);
-				
 				if(str.includes("@"))//We have an email address. It's not a 300 character regex, but it works for this input.
 				{
-					exportStr += addr[20] + ",";
+					exportStr += addr[20] + "\n";
+					continue; //We want to only execute the links once all emails are done.
 				}
 			}  
 		}
-		//Delete the last comma.
-		exportStr = exportStr.substr(0, exportStr.length -1);
-		var blob = new Blob([exportStr], {
-			type: "text/plain;charset=utf-8;",
-		});
-		saveAs(blob, "emails.csv");
-		alert("Attention: please make sure to run this script on every page of housemates. Mailing lists can be made form this script as asw.iastate.edu");
-	});
  }
  
  
