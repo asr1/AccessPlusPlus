b524da1fee9fd06052f616b475eed035894ecb4a
asr1 <alexandersrinehart@gmail.com>
2016-02-20 00:14:22 -0600

    Well, it *works*

--- emailExport.js
+++ emailExport.js
@@ -3,6 +3,8 @@
 var url =  window.location.href;
 var accessPlus = "https://accessplus.iastate.edu/RH05/RH958"; //possible url for access plus after first access
 var accessPlus2 = "https://accessplus.iastate.edu/servlet/adp.A_Plus";
+
+var table, exportStr;
  if (url.includes(accessPlus) || url == accessPlus2)
  {
 	$("body").prepend('<input type="button" id="exportBtn" value="Export Emails">');
@@ -10,32 +12,31 @@ var accessPlus2 = "https://accessplus.iastate.edu/servlet/adp.A_Plus";
 		$("#exportBtn").click(function(){
 
 		//Iterate through each cell in the third row
-		var table = $("table:visible"); 
+		table = $("table:visible"); 
 		table = table[7];
 		var done = false;
 		
 		
-		var exportStr = "";
+		exportStr = "";
 		
 	
 		checkPage();
 
 		//Once we're done with all links, let's see if the page is done.
 		
-		//Iterate through each link
-		$("a").each(function() {
-		 	if($(this).context.innerHTML.includes("Next"))
-			{
-				console.log("hit");
-				console.log($(this));
-				alert($(this).context.innerHTML);
-				$(this).click();
-		})
+		// //Iterate through each link
+		// $("a").each(function() {
+		 	// if($(this).context.innerHTML.includes("Next"))
+			// {
+				// console.log($(this));
+				// $(this).click();
+			// }
+		// })
 
 		//Check the second page
 		//This is so kludgey I'm sorry.
 		//It's better than what we had before, I promsie.
-		checkPage();
+		//checkPage();
 		
 		
 		//Download the result
@@ -43,7 +44,7 @@ var accessPlus2 = "https://accessplus.iastate.edu/servlet/adp.A_Plus";
 			type: "text/plain;charset=utf-8;",
 		});
 			saveAs(blob, "emails.csv");
-		alert("Attention: please make sure to run this script on every page of housemates. Mailing lists can be made form this script as asw.iastate.edu");
+		alert("Attention: please make sure to run this script on every page of housemates. Mailing lists can be made form this script as asw.iastate.edu");//This is the best we can do?
 	});
  }
  
