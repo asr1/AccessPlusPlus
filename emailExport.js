193dbf1206939d04d402d9dbb03c9b3c5c0d392a
asr1 <alexandersrinehart@gmail.com>
2016-02-20 15:06:00 -0600

    Gave up on exports

--- emailExport.js
+++ emailExport.js
@@ -4,6 +4,8 @@ var url =  window.location.href;
 var accessPlus = "https://accessplus.iastate.edu/RH05/RH958"; //possible url for access plus after first access
 var accessPlus2 = "https://accessplus.iastate.edu/servlet/adp.A_Plus";
 
+
+var done = false;
 var table, exportStr;
  if (url.includes(accessPlus) || url == accessPlus2)
  {
@@ -32,28 +34,23 @@ var table, exportStr;
 			{
 				console.log($(this));
 				window.location.href = $(this).context.href;
-				//This is happening too soon, need to wait.
-				//Can we do like a secon document.ready?
-				checkPage();  
-				download();
-	
-			//	checkPage();//Hope they don't have three pages.
+				//Chrome hates timeouts for some reason, have to run on every page :/
 				return false; //Break statement
-				//$(this).click();//This is the "next page" button, but it's not taking us there.
 			}
 		})
+		download();
 	});
  }
  
  function download()
  {
+	 		alert("Attention: please make sure to run this script on every page of housemates. Mailing lists can be made form this script as asw.iastate.edu");//This is the best we can do.
 	 	//Download the result
 		var blob = new Blob([exportStr], {
 			type: "text/plain;charset=utf-8;",
 		});
-			saveAs(blob, "emails.csv");
+			saveAs(blob, "emails.txt");
  }
- 
  
  function checkPage()
  {
@@ -65,7 +62,7 @@ var table, exportStr;
 				var addr = str.split(" ");
 				if(str.includes("@"))//We have an email address. It's not a 300 character regex, but it works for this input.
 				{
-					exportStr += addr[20] + "\n";
+					exportStr += addr[20] + "\r\n";
 					continue; //We want to only execute the links once all emails are done.
 				}
 			}  
